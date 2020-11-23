a9d9906fbfa216211a559cbc45998854a3e9bd9f
Chris Toxiadis <chris.it185288@gmail.com>
2020-11-23 20:42:22 +0200

    minor change

--- src/data_base_interface/DBConnection.java
+++ src/data_base_interface/DBConnection.java
@@ -11,7 +11,7 @@ import java.util.Vector;
 
 public class DBConnection {
     static String driver = "org.postgresql.Driver";
-    static String url = "jdbc:postgresql://dblabs.it.teithe.gr:5432/it185288";
+    static String url = "jdbc:postgresql://dblabs.it.teithe.gr:5432";
     static Connection dbcon = null;
     static Statement state = null;
     static ResultSet res = null;
