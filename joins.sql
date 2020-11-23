bfe0c73c03c48f7cdc8a740cc42394981ee43360
vaggos542 <37916133+vaggos542@users.noreply.github.com>
2020-11-23 02:47:57 +0200

    Add files via upload

---
+++ joins.sql
@@ -0,0 +1,17 @@
+﻿----------------------------------------------------------------------------------------------
+SELECT r.idcars,r.brand,c.firstname FROM carshop.cars AS r 
+JOIN carshop.sales AS s ON r.idcars = s.idcars 
+JOIN carshop.customers AS c ON s.idcustomers = c.idcustomers WHERE firstname = 'Leonardo'
+----------------------------------------------------------------------------------------------
+SELECT r.idcars,r.brand,c.firstname FROM carshop.cars AS r
+JOIN carshop.sales AS s ON r.idcars = s.idcars 
+JOIN carshop.customers AS c ON s.idcustomers = c.idcustomers  WHERE r.brand = 'Tesla'
+----------------------------------------------------------------------------------------------
+SELECT r.idcars,r.color,c.firstname FROM carshop.cars AS r
+JOIN carshop.sales AS s ON r.idcars = s.idcars 
+JOIN carshop.customers AS c ON s.idcustomers = c.idcustomers  WHERE r.color = 'Silver'
+----------------------------------------------------------------------------------------------
+SELECT r.idcars,r.brand,c.firstname,s.date FROM carshop.cars AS r
+JOIN carshop.sales AS s ON r.idcars = s.idcars 
+JOIN carshop.customers AS c ON s.idcustomers = c.idcustomers  WHERE s.date = '8/11/2020'
+----------------------------------------------------------------------------------------------
